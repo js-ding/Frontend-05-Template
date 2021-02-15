2a5f73a06296c55eae180e3a9cf200ae7b22376d
gongding <15908606719@163.com>
2021-02-15 20:27:29 +0800

    feat: 第十九周

--- Week 19/task/publish-server/server.js
+++ Week 19/task/publish-server/server.js
@@ -1,12 +1,90 @@
 const http = require('http');
+const https = require('https');
 // const fs = require('fs');
 const unzipper = require('unzipper');
+const querystring = require('querystring');
+const { info } = require('console');
+
+// 2. auth路由：接收code，换取token回传给客户端
+function auth(request, response) {
+    const query = querystring.parse(request.url.match(/^\/auth\?([\s\S]+)$/)[1]);
+    getToken(query.code, (body) => {
+        // 返回一个发布页面
+        response.write(`<a href=http://localhost:3303/publish?token=${body.access_token}>publish</a>`);
+        response.end();
+    });
+}
+
+function getToken(code, callback) {
+    const request = https.request({
+        hostname: 'github.com',
+        port: 443,
+        path: `/login/oauth/access_token?code=${code}&client_id=Iv1.1c055c1d55454bd2&client_secret=c1b7fd42f936dbca44c32290de8ac6f9bfc70597`,
+        method: 'POST',
+    }, function(response) {
+        let body = '';
+        response.on('data', chunk => {
+            body += chunk;
+        });
+        response.on('end', () => {
+            callback(querystring.parse(body));
+        })
+    });
+    request.end();
+}
+
+// 4. publish路由：用token获取用户信息，检查权限，接受发布
+function publish(request, response) {
+    const query = querystring.parse(request.url.match(/^\/publish\?([\s\S]+)$/)[1]);
+    if (query.token) {
+        getUser(query.token, function(info) {
+            // Request forbidden by administrative rules. Please make sure your request has a User-Agent header (http://developer.github.com/v3/#user-agent-required). Check https://developer.github.com for other possible causes.:''
+            // console.log(body);
+            // {"login":"markgong-gd","id":48786701,"node_id":"MDQ6VXNlcjQ4Nzg2NzAx","avatar_url":"https://avatars.githubusercontent.com/u/48786701?v:'4","gravatar_id":"","url":"https://api.github.com/users/markgong-gd","html_url":"https://github.com/markgong-gd","followers_url":"https://api.github.com/users/markgong-gd/followers","following_url":"https://api.github.com/users/markgong-gd/following{/other_user}","gists_url":"https://api.github.com/users/markgong-gd/gists{/gist_id}","starred_url":"https://api.github.com/users/markgong-gd/starred{/owner}{/repo}","subscriptions_url":"https://api.github.com/users/markgong-gd/subscriptions","organi…rl":"https://api.github.com/users/markgong-gd/repos","events_url":"https://api.github.com/users/markgong-gd/events{/privacy}","received_events_url":"https://api.github.com/users/markgong-gd/received_events","type":"User","site_admin":false,"name":"dinggong","company":null,"blog":"","location":null,"email":null,"hireable":null,"bio":null,"twitter_username":null,"public_repos":33,"public_gists":0,"followers":3,"following":7,"created_at":"2019-03-21T07:52:32Z","updated_at":"2021-02-14T14:14:56Z"}'
+            if (info.login === 'markgong-gd') {
+                request.pipe(unzipper.Extract({ path: '../server/public' }));
+                request.on('end', () => {
+                    response.end('success!');
+                })
+            }
+        });
+    }
+
+}
+
+function getUser(token, callback) {
+    const request = https.request({
+        hostname: 'api.github.com',
+        port: 443,
+        path: '/user',
+        method: 'GET',
+        headers: {
+            'Authorization': `token ${token}`,
+            'User-Agent': 'markgong-gd'
+        }
+    }, function(response) {
+        let body = '';
+        response.on('data', chunk => {
+            body += chunk;
+        });
+        response.on('end', () => {
+            callback(JSON.parse(body));
+        })
+    });
+    request.end();
+}
 
 http.createServer((request, response) => {
+    if (request.url.match(/^\/auth/) && request.url.match(/^\/auth/)[0] === '/auth') {
+        return auth(request, response);
+    }
+    if (request.url.match(/^\/publish/) && request.url.match(/^\/publish/)[0] === '/publish') {
+        return publish(request, response);
+    }
     // 这里服务系统和发布系统存放在一台机器上，这里直接使用相对路径
     // const outFile = fs.createWriteStream('../server/public/index.html');
 
-    request.pipe(unzipper.Extract({ path: '../server/public' }));
+    // request.pipe(unzipper.Extract({ path: '../server/public' }));
 
     /*request.on('data', chunk => {
         outFile.write(chunk);
@@ -16,3 +94,4 @@ http.createServer((request, response) => {
         response.end('Success');
     });*/
 }).listen(3303);
+
--- Week 19/task/publish-tool/publish.js
+++ Week 19/task/publish-tool/publish.js
@@ -1,6 +1,8 @@
 const http = require('http');
 const fs = require('fs');
 const archiver = require('archiver');
+const child_process = require('child_process');
+const querystring = require('querystring');
 
 /*  ============================= 1. 读取流 ===============================
 const file = fs.createReadStream('./package.json');
@@ -46,14 +48,50 @@ file.on('end', chunk => {
 // });
 
 // =================== 3. 多文件打包上传 =======================
-fs.stat('./sample.html', (err, stats) => {
+// fs.stat('./sample.html', (err, stats) => {
+//     const request = http.request({
+//         // hostname: '47.100.95.127',
+//         hostname: '127.0.0.1',
+//         port: 3303,
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/octet-stream'
+//         }
+//     }, response => {
+//         console.log(response);
+//     });
+
+//     const archive = archiver('zip', {
+//         zlib: { level: 9 } // Sets the compression level.
+//     });
+
+//     archive.directory('./sample/', false);
+
+//     // pipe archive data to the file
+//     archive.pipe(request);
+
+//     // finalize the archive (ie we are done appending files but streams have to finish yet)
+//     // 'close', 'end' or 'finish' may be fired right after calling this method so register to them beforehand
+//     archive.finalize();
+// });
+// ================= 接入github oauth ============================
+// 1. GET https://github.com/login/oauth/authorize 获取code，自家后台拿取code
+child_process.exec(`start https://github.com/login/oauth/authorize?client_id=Iv1.1c055c1d55454bd2`);
+
+// 3. 创建server，接收token，点击发布
+http.createServer((request, response) => {
+    const query = querystring.parse(request.url.match(/^\/auth\?([\s\S]+)$/)[1]);
+    publish(query.token);
+})
+
+function publish(token) {
     const request = http.request({
-        // hostname: '47.100.95.127',
-        hostname: '127.0.0.1',
-        port: 3303,
-        method: 'POST',
+        hostname: 'localhost',
+        port: '3303',
+        path: `/publish?token=${token}`,
+        method: 'GET',
         headers: {
-            'Content-Type': 'application/octet-stream'
+            'Content-Type': 'application/octet-stream',
         }
     }, response => {
         console.log(response);
@@ -62,13 +100,16 @@ fs.stat('./sample.html', (err, stats) => {
     const archive = archiver('zip', {
         zlib: { level: 9 } // Sets the compression level.
     });
-
+    
     archive.directory('./sample/', false);
-
+    
     // pipe archive data to the file
     archive.pipe(request);
-
+    
     // finalize the archive (ie we are done appending files but streams have to finish yet)
     // 'close', 'end' or 'finish' may be fired right after calling this method so register to them beforehand
     archive.finalize();
-});
+}
+
+
+
